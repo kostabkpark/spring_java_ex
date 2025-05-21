bec278f655e6f012942af17c890ce428b213d149
kostabkpark <kostabkpark@gmail.com>
2025-05-21 12:39:34 +0900

    java solid ocp

--- src/test/Main_Post.java
+++ src/test/Main_Post.java
@@ -3,9 +3,19 @@ package test;
 public class Main_Post {
   public static void main(String[] args) {
     Post post = new NewPost();
-    PrintPost printPost = new PrintLinePost(post);
+    //PrintPost printPost = new PrintLinePost(post);
+    PrintPost printPost = new PrintNoLinePost(post);
     printPost.print();
     printPost.print();
+
+//    PrintLinePost printLinePost = new PrintLinePost();
+//    printLinePost.setPost(post);
+//    printLinePost.print();
+//
+//    PrintNoLinePost printNoLinePost = new PrintNoLinePost();
+//    printNoLinePost.setNoLinePost(post);
+//    printNoLinePost.print();
+//    printNoLinePost.println();
   }
 }
 
@@ -24,14 +34,42 @@ interface PrintPost {
   void print();
 }
 
+//@컴포넌트로 생성해줘
 class PrintLinePost implements PrintPost {
   private Post post;
-
+  public PrintLinePost() {}
   public PrintLinePost(Post post) {
     this.post = post;
   }
+
+  public void setPost(Post post) {
+    this.post = post;
+  }
+
   @Override
   public void print() {
+    System.out.println(post.getAllPosts());
+  }
+}
+
+//@컴포넌트로 생성해줘
+class PrintNoLinePost implements PrintPost {
+  private Post post;
+  public PrintNoLinePost() {}
+  public PrintNoLinePost(Post post) {
+    this.post = post;
+  }
+
+  public void setNoLinePost(Post post) {
+    this.post = post;
+  }
+
+  @Override
+  public void print() {
+    System.out.print(post.getAllPosts());
+  }
+
+  public void println() {
     System.out.println(post.getAllPosts());
   }
 }
